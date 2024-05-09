f430128bbf6bc59a2b6c3e080c121197e4d2af23
kryptokopytko <kryptokopytko921@gmail.com>
2024-05-09 15:15:43 +0200

    Fix Challenges Views

--- Views/src/Components/AttractionsList.tsx
+++ Views/src/Components/AttractionsList.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { List, ListItem, ListItemText } from '@mui/material';
+import { Attraction } from '../types'; // Importujemy interfejs Attraction
 
 interface ListProps {
-  items: string[];
+  items: Attraction[]; // Zmiana typu listy na Attraction[]
 }
 
 const AttractionsList: React.FC<ListProps> = ({ items }) => {
   return (
     <List>
-      {items.map((item, index) => (
+      {items.map((attraction, index) => (
         <ListItem key={index}>
-          <ListItemText primary={item} />
+          {/* Wyświetlamy nazwę atrakcji */}
+          <ListItemText primary={attraction.name} />
         </ListItem>
       ))}
     </List>
