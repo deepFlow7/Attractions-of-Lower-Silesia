873f4c79cc926f9c224ea8f7a9ff27232c45efb3
kryptokopytko <kryptokopytko921@gmail.com>
2024-05-17 19:58:52 +0200

    Style Challenge

--- client/src/Components/Challenge.tsx
+++ client/src/Components/Challenge.tsx
@@ -1,15 +1,64 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
 import React from 'react';
+import { Card, CardContent, Grid, Typography } from '@mui/material';
 import Map from './Map';
 import AttractionsList from './AttractionsList';
 import RankingTable from './Ranking';
 import { Challenge, Ranking } from '../types';
 
-const ChallengeView: React.FC<{ rankings: Ranking[]; challenge: Challenge }> = ({ rankings, challenge }) => {
+interface ChallengeViewProps {
+  rankings: Ranking[];
+  challenge: Challenge;
+}
+
+const Container = styled.div`
+  padding: 20px;
+`;
+
+const Section = styled(Card)`
+  margin-bottom: 20px;
+`;
+
+const Title = styled(Typography)`
+  text-align: center;
+  font-weight: bold;
+  margin-bottom: 16px;
+`;
+
+const ChallengeView: React.FC<ChallengeViewProps> = ({ rankings, challenge }) => {
+  const x = 51.1079;
+  const y = 17.0385;
+
   return (
-    <div>
-      <AttractionsList items={challenge.attractions} />
-      <RankingTable rankings={rankings} />
-    </div>
+    <Container>
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={5}>
+          <Section>
+            <CardContent>
+              <Title variant="h5">Mapa</Title>
+              <Map x={x} y={y} />
+            </CardContent>
+          </Section>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Section>
+            <CardContent>
+              <AttractionsList items={challenge.attractions} />
+            </CardContent>
+          </Section>
+        </Grid>
+        <Grid item xs={12} md={3}>
+          <Section>
+            <CardContent>
+              <Title variant="h5">Ranking</Title>
+              <RankingTable rankings={rankings} />
+            </CardContent>
+          </Section>
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
