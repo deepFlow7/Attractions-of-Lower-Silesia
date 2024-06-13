174811def6fdbebd7b954a2568c76702b6d9c38c
kryptokopytko <kryptokopytko921@gmail.com>
2024-06-13 16:19:30 +0200

    Make Filter Work

--- client/src/Components/Home.tsx
+++ client/src/Components/Home.tsx
@@ -1,12 +1,12 @@
 /** @jsxImportSource @emotion/react */
 import styled from '@emotion/styled';
-import React,{useEffect,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Grid, Card } from '@mui/material';
-import Filter from './Filter';
 import Map from './Map';
 import AttractionsList from './AttractionsList';
-import { Attraction } from '../types';
+import { possibleSubtypes, possibleTypes, Attraction, possible_type, subtypes } from '../types';
+import FilterList from './FilterList';
 
 interface HomeProps {
 }
@@ -20,27 +20,31 @@ const Container = styled.div`
 
 
 const Home: React.FC<HomeProps> = () => {
-    const filterOptions=["ladne", "nieladne", "a"];
   const x = 51.1079;
   const y = 17.0385;
-  const [attractions,setAttractions] = useState<Attraction[]|null>(null);
+  const [attractions, setAttractions] = useState<Attraction[] | null>(null);
+  const [filteredAttractions, setFilteredAttractions] = useState<Attraction[]>([]);
 
-    useEffect(() => {
-        axios.get('/api/attractions')
-            .then(response => {
-            setAttractions(response.data);
-            })
-            .catch(error => {
-            console.error('There was an error fetching the data!', error);
-            });
-    }, []);
+  useEffect(() => {
+    axios.get('/api/attractions')
+      .then(response => {
+        setAttractions(response.data);
+        setFilteredAttractions(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the data!', error);
+      });
+  }, []);
 
-    if(!attractions){
-        return <div> Loading...</div>;
+  if (!attractions) {
+    return <div> Loading...</div>;
+  }
+
+  function handleFilterChange(selectedTypes: possible_type[], selectedSubtypes: subtypes[]) {
+    if (attractions != null) {
+      setFilteredAttractions(attractions.filter(a => selectedSubtypes.includes(a.subtype) && selectedTypes.includes(a.type)))
+      alert("" + filteredAttractions[0].name)
     }
-
-  function handleFilterChange() {
-    // Tutaj można dodać logikę obsługi zmiany filtru, jeśli jest potrzebna
   }
 
   return (
@@ -48,19 +52,18 @@ const Home: React.FC<HomeProps> = () => {
       <Grid container spacing={2}>
         <Grid item xs={12} md={5}>
           <TileCard>
-            <Map x={x} y={y} attractions={attractions}/>
+            <Map x={x} y={y} attractions={attractions} />
           </TileCard>
         </Grid>
 
         <Grid item xs={12} md={4}>
           <TileCard>
-            <AttractionsList attractions={attractions} type_filter={filterOptions}/>
+            <AttractionsList attractions={filteredAttractions}  />
           </TileCard>
         </Grid>
         <Grid item xs={12} md={3}>
           <TileCard>
-            <Filter
-              options={filterOptions}
+            <FilterList
               onChange={handleFilterChange}
             />
           </TileCard>
