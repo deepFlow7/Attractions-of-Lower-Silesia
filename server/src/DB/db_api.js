7c2e157b3af0db8fd318787a39a83b8cc19a03a5
Qwertyizm <stanislaw.sochacki@outlook.com>
2024-07-15 18:58:21 +0200

    Sprint 7 task 3
    Adjust database info for both development and deployment

--- server/src/DB/db_api.js
+++ server/src/DB/db_api.js
@@ -3,10 +3,10 @@ var pg = require('pg');
 var bcrypt = require('bcrypt');
 
 const pool = new pg.Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS,
+    user: process.env.DB_USER || 'pg',
+    host: process.env.DB_HOST || 'localhost',
+    database: process.env.DB_NAME || 'maps',
+    password: process.env.DB_PASS || 'pg',
     port: 5432,
   });
 
