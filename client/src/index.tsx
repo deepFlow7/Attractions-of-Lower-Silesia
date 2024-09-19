10f1b3fe630f0e2914edfc14e98fab009d1fb364
Qwertyizm <stanislaw.sochacki@outlook.com>
2024-09-19 17:38:46 +0200

    Add lazy imports for routes #1102

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import StyledNavbar from './Components/Navbar';
-import Home from './Components/Home';
-import Attraction from "./Components/Attraction";
-import ChallengeView from './Components/Challenge';
-import Challenges from './Components/Challenges';
-import NewAttractionForm from './Components/NewAttractionForm';
-import SignUpForm from './Components/SignUpForm';
-import LoginForm from './Components/LoginForm';
-import NewChallengeForm from './Components/NewChallengeForm';
 import { AuthProvider, useAuth } from './Providers/AuthContext';
-import RoutePlanner from './Components/RoutePlanner';
-import AdminView from './Components/AdminView';
+
+const paths = ["Home",]
+const Home = lazy(() => import('./Components/Home'));
+const Attraction = lazy(() => import('./Components/Attraction'));
+const ChallengeView = lazy(() => import('./Components/Challenge'));
+const Challenges = lazy(() => import('./Components/Challenges'));
+const NewAttractionForm = lazy(() => import('./Components/NewAttractionForm'));
+const SignUpForm = lazy(() => import('./Components/SignUpForm'));
+const LoginForm = lazy(() => import('./Components/LoginForm'));
+const NewChallengeForm = lazy(() => import('./Components/NewChallengeForm'));
+const RoutePlanner = lazy(() => import('./Components/RoutePlanner'));
+const AdminView = lazy(() => import('./Components/AdminView'));
 
 const ProtectedRoute = ({ element }: { element: JSX.Element }) => {
   const { isAuthenticated, role } = useAuth();
@@ -35,17 +37,19 @@ root.render(
     <AuthProvider>
       <BrowserRouter>
         <StyledNavbar />
-        <Routes>
-          <Route path="/" element={<RootRedirect />} />
-          <Route path="/attraction/:attractionId" element={<Attraction />} />
-          <Route path="/challenge/:challengeId" element={<ChallengeView />} />
-          <Route path="/challenges" element={<Challenges />} />
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="/new_attraction" element={<ProtectedRouteForUnblocked element={<NewAttractionForm />} />} />
-          <Route path="/new_challenge" element={<ProtectedRoute element={<NewChallengeForm />} />} />
-          <Route path="/signup" element={<SignUpForm />} />
-          <Route path="/route_planner" element={<RoutePlanner />} />
-        </Routes>
+        <Suspense fallback={<>loading...</>}>
+          <Routes>
+            <Route path="/" element={<RootRedirect />} />
+            <Route path="/attraction/:attractionId" element={<Attraction />} />
+            <Route path="/challenge/:challengeId" element={<ChallengeView />} />
+            <Route path="/challenges" element={<Challenges />} />
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/new_attraction" element={<ProtectedRouteForUnblocked element={<NewAttractionForm />} />} />
+            <Route path="/new_challenge" element={<ProtectedRoute element={<NewChallengeForm />} />} />
+            <Route path="/signup" element={<SignUpForm />} />
+            <Route path="/route_planner" element={<RoutePlanner />} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </AuthProvider>
   </React.StrictMode>
